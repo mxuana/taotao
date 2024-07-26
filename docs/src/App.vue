278ed45c0bd5bf25046a2c1d3d00041c13531e8d
Oxrial <35791730+Oxrial@users.noreply.github.com>
2024-07-26 22:12:59 +0800

    udpate 8

--- docs/src/App.vue
+++ docs/src/App.vue
@@ -4,6 +4,10 @@
 
 <script setup>
 import HOME from './views/home/index.vue'
+const { useRoute, useRouter } = window['VueRouter']
+const route = useRoute()
+const router = useRouter()
+console.log(route, router)
 </script>
 
 <style lang="scss" scoped></style>
