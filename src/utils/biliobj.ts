38e4a6c4981af21034e1883666800e3027f3456e
Oxrial <35791730+Oxrial@users.noreply.github.com>
2024-07-31 17:07:25 +0800

    update bilibili api

---
+++ src/utils/biliobj.ts
@@ -0,0 +1,49 @@
+export class Song {
+	id: string = ''
+	bvid: string = ''
+	name: string = ''
+	singer: string = ''
+	singerId: string = ''
+	cover: string = ''
+	musicSrc: string = ''
+	lyric: string = ''
+	lyricOffset: string = ''
+	constructor({ cid, bvid, name, singer, cover, musicSrc, singerId, lyric, lyricOffset }: any) {
+		this.id = cid
+		this.bvid = bvid
+		this.name = name
+		this.singer = singer
+		this.singerId = singerId
+		this.cover = cover
+		this.musicSrc = musicSrc
+		this.lyric = lyric
+		this.lyricOffset = lyricOffset
+	}
+}
+type Page = { bvid: string; part: string; cid: string }
+export class VideoInfo {
+	title: string = ''
+	desc: string = ''
+	videos: number = 0
+	picSrc: string = ''
+	uploader: string = ''
+	pages: Page[] = []
+	constructor(title: string, desc: string, videos: number, picSrc: string, uploader: string, pages: Page[]) {
+		this.title = title
+		this.desc = desc
+		this.videos = videos
+		this.picSrc = picSrc
+		this.uploader = uploader
+		this.pages = pages
+	}
+
+	isMutiPartVideo() {
+		return this.videos > 1
+	}
+
+	getMultiPartVideoList() {}
+}
+export default {
+	Song,
+	VideoInfo
+}
