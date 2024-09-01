941f540aa206b7a797715362bcc4205cc720b2c8
oxrial <1972416817@qq.com>
2024-09-02 00:02:31 +0800

    update style1

---
+++ src/assets/mat.ts
@@ -0,0 +1,5 @@
+// import fs from 'fs'
+// import path from 'path'
+// import songs from './songs'
+// console.log(songs.length)
+// fs.writeFileSync(path.join('src', 'assets', 'data.json'), JSON.stringify([]))
